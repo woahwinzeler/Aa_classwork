a4a3121cb366e28c0e04c34c0da46b738479c130
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-09-21 14:34:16 -0700

    bugs squashed

--- w4/d2/project_1/Employee.rb
+++ w4/d2/project_1/Employee.rb
@@ -1,4 +1,4 @@
-require "Manager.rb"
+#require_relative './Manager.rb'
 
 class Employee
 
@@ -19,11 +19,11 @@ end
 
 
 
-Ned = Manager.new("Ned", "Founder", 1000000, nil, ["Darren", "Shawna", "David"])
-Darren = Manager.new("Darren", "TA Manager", 78000, "Ned", ["Shawna", "David"])
-Shawna = Employee.new("Shawna", "TA", 12000, "Darren")
-David = Employee.new("David", "TA", 10000, "Darren")
+# # Ned = Manager.new("Ned", "Founder", 1000000, nil, ["Darren", "Shawna", "David"])
+# # Darren = Manager.new("Darren", "TA Manager", 78000, "Ned", ["Shawna", "David"])
+# Shawna = Employee.new("Shawna", "TA", 12000, "Darren")
+# David = Employee.new("David", "TA", 10000, "Darren")
 
-p ned.bonus(5) # => 500_000
-p darren.bonus(4) # => 88_000
-p david.bonus(3) # => 30_000
+# p ned.bonus(5) # => 500_000
+# p darren.bonus(4) # => 88_000
+# p david.bonus(3) # => 30_000
--- w4/d2/project_1/Manager.rb
+++ w4/d2/project_1/Manager.rb
@@ -1,9 +1,9 @@
-require "Employee.rb"
+require_relative "Employee.rb"
 
 class Manager < Employee
-
-    def initialize(employees)
-        super
+    attr_reader :employees
+    def initialize(name, title, salary, boss = nil, employees)
+        super(name, title, salary, boss)
         @employees = employees 
     end
 
@@ -14,14 +14,15 @@ class Manager < Employee
         end
         sum * multiplier 
     end
-
 end
 
 
-Ned = Manager.new("Ned", "Founder", 1000000, nil, ["Darren", "Shawna", "David"])
-Darren = Manager.new("Darren", "TA Manager", 78000, "Ned", ["Shawna", "David"])
-Shawna = Employee.new("Shawna", "TA", 12000, "Darren")
-David = Employee.new("David", "TA", 10000, "Darren")
+
+shawna = Employee.new("Shawna", "TA", 12000, "Darren")
+david = Employee.new("David", "TA", 10000, "Darren")
+darren = Manager.new("Darren", "TA Manager", 78000, "Ned", [shawna, david])
+ned = Manager.new("Ned", "Founder", 1000000, nil, [darren, shawna, david])
+
 
 p ned.bonus(5) # => 500_000
 p darren.bonus(4) # => 88_000
