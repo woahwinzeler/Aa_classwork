9712e1bcd4bfbbd5d357807f908ad990af124f50
tharpercp <tharpercp@gmail.com>
2021-09-21 15:14:37 -0700

    finished initial set up

--- w4/d2/chess/board.rb
+++ w4/d2/chess/board.rb
@@ -2,48 +2,56 @@ require_relative "./piece.rb"
 require 'byebug'
 
 class Board
-  attr_reader :rows
+  attr_reader :board
 
   def self.build_board
-    @rows.each_with_index do |row, file|
-      row.each do |tile|
-        if file < 3 or file > 5
-          tile = Piece.new
-        end 
+    rows = Array.new(8){Array.new(8)}
+    rows.each_with_index do |row, file|
+      row.each_with_index do |tile, idx|
+        if file < 2 or file > 5
+          rows[file][idx] = :A
+        else
+          rows[file][idx] = :O
+        end
       end
     end
+    rows 
   end
 
-  def initialize()
-    @rows = Array.new(8){Array.new(8)}
+  def initialize
+    @board = Board.build_board 
     
     # private 
     # @sentinal = NullPiece
   end
 
-  def move_piece(start, end_pos)
-    if valid_pos?(end_pos)
-      @row[start] = nil
-      @row[end_pos] = self
+  def move_piece(start, end_pos, piece = :X)
+    if valid_move?(end_pos)
+      x, y = start
+      i, j = end_pos
+      @board[x][y] = nil
+      @board[i][j] = piece
     end 
   end
 
   def add_piece(piece, pos)
-    @rows[pos] = piece 
+    @board[pos] = piece 
   end
 
   def [](pos)
     x, y = pos
-    @rows[x][y] 
+    @board[x][y] 
   end
 
   def []=(pos, value)
     x, y = pos
-    @rows[x][y] = value 
+    @board[x][y] = value 
   end
 
-  def valid_pos?(pos)
-    #not done 
-    x, y = pos
+  def valid_move?(start_pos, end_pos)
+    pos = start_pos + end_pos
+    pos.each { |x| return false if x < 0 || x > 7 } #checking if its on board
+    return false if start_pos.nil? #is there a piece at start pos? End pos later
+    true 
   end
 end
