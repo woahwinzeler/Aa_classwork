2458cf937449e985bc205143d99436deb894d895
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-09-27 11:49:35 -0700

    Progress on p01 [ar]

--- w5/D1/skeleton/lib/p01_int_set.rb
+++ w5/D1/skeleton/lib/p01_int_set.rb
@@ -34,23 +34,29 @@ end
 
 
 class IntSet
+  attr_reader :num_buckets
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -59,26 +65,42 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
+    # @store.length has to be > @count
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
   def insert(num)
+    if !include?(num)
+      self[num] << num
+      @count += 1
+      if @count == num_buckets
+        resize!
+      end
+    end
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    else
+      return nil
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+     @store[num % num_buckets]
   end
 
   def num_buckets
@@ -86,5 +108,14 @@ class ResizingIntSet
   end
 
   def resize!
+    new_int_set = ResizingIntSet.new(num_buckets * 2)
+    @count = 0
+    @store.each do |bucket|
+      bucket.each do |ele|
+        new_int_set.store[ele % (num_buckets * 2)] << ele
+        @count += 1
+      end
+    end
+    @store = new_int_set.store
   end
 end
