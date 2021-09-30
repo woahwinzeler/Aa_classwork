938b90bbd3d36c86a83006ea99787afdac578a60
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-09-30 15:39:57 -0700

    basic associations

--- w5/d4/skeleton/app/models/user.rb
+++ w5/d4/skeleton/app/models/user.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: :Enrollment
+  
+  
+  
 end
