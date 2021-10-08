d2c02a3e8396e08593b79dc63fe07c3e34e76513
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-08 16:07:25 -0700

    Cats new progress [ar]

--- w6/d5/Cats99/app/controllers/cats_controller.rb
+++ w6/d5/Cats99/app/controllers/cats_controller.rb
@@ -12,4 +12,20 @@ class CatsController < ApplicationController
   def new 
     render :new 
   end
+
+  def create
+    cat = Cat.new(cat_params)
+
+    if cat.save
+      redirect_to cat_url(cat)
+    else
+      # render :json cat.errors.full_messages, status: 422
+    end
+  end
+
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :name, :sex, :description, :color)
+  end
 end
