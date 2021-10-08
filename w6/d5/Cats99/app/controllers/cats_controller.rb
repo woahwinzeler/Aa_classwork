a3a34010bdcb67be522495fb679fff330a851837
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-08 15:10:28 -0700

    Progress on db [ar]

--- w6/d5/Cats99/app/controllers/cats_controller.rb
+++ w6/d5/Cats99/app/controllers/cats_controller.rb
@@ -1,10 +1,11 @@
 class CatsController < ApplicationController
   def index
+    @cats = Cat.all
     render :index 
   end
 
   def show
-    @c = Cat.find_by(id: parmas[:id])
+    @c = Cat.find_by(id: params[:id])
     render :show 
   end
 end
--- w6/d5/Cats99/db/seeds.rb
+++ w6/d5/Cats99/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.create(birth_date: "2015-01-20", name: 'John', sex: 'M', color: 'Purple')
+
+Cat.create(birth_date: "2015-03-20", name: 'Milo', sex: 'M', color: 'Brown')
+
+Cat.create(birth_date: "2015-05-20", name: 'Lila', sex: 'F', color: 'Black')
+
+Cat.create(birth_date: "2015-01-24", name: 'Spot', sex: 'M', color: 'Black')
+
+Cat.create(birth_date: "2016-01-20", name: 'Wally', sex: 'M', color: 'Brown')
+
