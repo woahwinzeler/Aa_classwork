05490f104d6dace11e38f640d01639d83f0b3279
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-10-08 16:43:24 -0700

    Added age and progressed on new

--- w6/d5/Cats99/app/models/cat.rb
+++ w6/d5/Cats99/app/models/cat.rb
@@ -3,4 +3,18 @@ class Cat < ApplicationRecord
   validates :sex, inclusion: {in: ['M', 'F']}
   validates :color, inclusion: {in: ['Brown', 'Black', 'White', 'Purple']}
 
+  include ActionView::Helpers::DateHelper
+
+  def age
+    # distance_of_time_in_words(birth_date, Time.now).grep(/\d/)
+
+    age = Time.now.year - birth_date.year 
+    if age < 1
+      age = (Time.now.month - birth_date.month).to_s 
+      return age + " Months"
+    else
+      age = age.to_s
+      return age + " Years"
+    end
+  end
 end
