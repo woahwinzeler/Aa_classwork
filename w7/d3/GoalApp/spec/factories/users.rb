612b34487d7f1d181863b1a46119bc765326c8ea
user <jeffdplante@aol.com>
2021-10-13 16:27:36 -0700

    user model continues

--- w7/d3/GoalApp/spec/factories/users.rb
+++ w7/d3/GoalApp/spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+   username {Faker::Movies::HarryPotter.character}
+   password {"password:"}
   end
 end
--- w7/d3/GoalApp/spec/models/user_spec.rb
+++ w7/d3/GoalApp/spec/models/user_spec.rb
@@ -7,26 +7,64 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_presence_of(:session_token) }
-  it { should validate_uniqueness_of(:username)}
   it { should validate_length_of(:password).is_at_least(6) }
   
+  describe "uniqueness" do
+    
+    before :each do
+      create(:user)
+    end
+
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:session_token) }
+
+  end
+
   #Associations 
   it { should have_many(:goals)}
   it { should have_many(:comments)}
   it { should have_many(:comments_made)}
   
-  #methods SPIRE CRLLL
-  #self.find_by_credentials 
-  describe "finds users" do 
-    # let(:user1) {User.new(username: "John", password:"password")}
-    # let(:user2) {User.new(username: "Jill", password:"p4567d")}
-    user1 = User.create!(username: "Jonh", password: "password", session_token: "B")
-    user2 = User.create!(username: "Jill", password: "12345678", session_token: "C")
-    expect(user1.find_by_credentials("John", "password")).to eq(user1)
-    expect(user1.find_by_credentials("Jill", "12345678")).to eq(nil)
+  #methods
+  describe "is_valid_password?" do
+    let!(:user) { create(:user) }
 
-
+    context "with a valid password" do
+      it "should return true" do
+        expect(user.is_valid_password?("password")).to be true
+      end
+    end
+    context "with a invalid password" do
+      it "should return false" do
+        expect(user.is_valid_password?("spongebob")).to be false 
+      end
+    end
 
   end
+
+  describe "reset_session_token!" do
+    let!(:user) { create(:user) }
+
+    it "should change the session token" do
+      token = user.session_token
+      expect(user.reset_session_token!).to_not eq(token)      
+    end
+
+  end
+
+
+  #methods SPIRE CRLLL
+  #self.find_by_credentials 
+  # describe "finds users" do 
+  #   # let(:user1) {User.new(username: "John", password:"password")}
+  #   # let(:user2) {User.new(username: "Jill", password:"p4567d")}
+  #   # user1 = User.create!(username: "Jonh", password: "password", session_token: "B")
+  #   # user2 = User.create!(username: "Jill", password: "12345678", session_token: "C")
+  #   # expect(user1.find_by_credentials("John", "password")).to eq(user1)
+  #   # expect(user1.find_by_credentials("Jill", "12345678")).to eq(nil)
+
+
+
+  # end
 
 end
