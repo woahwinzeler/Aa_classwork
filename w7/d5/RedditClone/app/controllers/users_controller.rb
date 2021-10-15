56b1f122407f5f50b18edb594e3c491cb1e0e3f3
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-10-15 14:57:06 -0700

    application controller finished, validation error

--- w7/d5/RedditClone/app/controllers/users_controller.rb
+++ w7/d5/RedditClone/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, except: [:index, :new, :create]
+
   def index
     @users = User.all
     render :index
@@ -47,9 +49,29 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_url(user)
+    else
+      flash[:errors] = user.errors.full_messages 
+      render :new 
+    end
   end
 
   def update
+    user = User.find_by(id: params[:id])
+    if user
+      user.update(user_params)
+      
+    else
+      flash[:errors] = ["User not found"]
+      redirect_to users_url 
+    end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
--- w7/d5/RedditClone/app/models/user.rb
+++ w7/d5/RedditClone/app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   after_validation :ensure_session_token
 
   validates :username, :password_digest, :session_token, presence: true
-  validates :username, :session_token, uniquness: true
-  validates :password, length: {mimimum: 6}, allow_nil: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6}, allow_nil: true
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
@@ -22,7 +22,7 @@ class User < ApplicationRecord
 
   def password=(password)
     @password = password
-    password_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def is_valid_password?(password)
