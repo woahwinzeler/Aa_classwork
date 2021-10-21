ff6ddfbf2b0fcc876e7757dcdadf6327fabe1f70
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-10-20 17:02:03 -0700

    Phase 6 default hash

--- w8/d2/Intro JavaScript Exercises/phase_6_students_and_courses.js
+++ w8/d2/Intro JavaScript Exercises/phase_6_students_and_courses.js
@@ -12,11 +12,21 @@ Student.prototype.enroll = function (course) {
     if (!(this.courses.includes(course))) {
         this.courses.push(course);
         //update course list 
+        course.addStudent(this);
     } 
 }
 
 Student.prototype.courseLoad = function () {
-    // generate based on this.courses a hash where key: department name and value: credits 
+    // generate based on this courses a hash where key: department name and value: credits 
+    let courseload = new Proxy({}, {
+      get: function(obj, property){
+        return obj.hasOwnProperty(property) ? obj[property] : 0; 
+      }
+    })
+    this.courses.forEach(function(course){
+      courseload[course.department] += course.numCredits
+    });
+    return courseload; 
 }
 
 function Course (courseName, department, numCredits) {
@@ -26,6 +36,20 @@ function Course (courseName, department, numCredits) {
     this.students = [];
 }
 
+Course.prototype.addStudent = function(student){
+  this.students.push(student);
+}
+
+let Student1 = new Student("Owen", "Winzeler");
+let Student2 = new Student("Jacky", "Chen");
+
+let Course1 = new Course("English 101", "English", 4);
+let Course2 = new Course("AstroPhysics 244", "Math", 3);
+
+Student1.enroll(Course1)
+Student1.enroll(Course2)
+
+console.log(Student1.courseLoad())
 
 
 
