f83ec83aa72186e06b7ab52fd9b3659097aa9a18
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-11-09 17:07:29 -0800

    Finished day 1

--- w11/d2/Todo/frontend/reducers/stepsReducer.js
+++ w11/d2/Todo/frontend/reducers/stepsReducer.js
@@ -4,19 +4,20 @@ import makeIdentity from '../util/identify'
 
 const stepsReducer = (state={}, action) => {
   Object.freeze(state);
+  let newState = Object.assign({}, state);
   switch (action.type) {
      case RECEIVE_STEPS:
-      return Object.assign(makeIdentity(action.steps), {})
+      return Object.assign(newState, makeIdentity(action.steps));
      case RECEIVE_STEP:
       const newReturnState = Object.assign({}, state);
-     newReturnState[action.step.id] = action.step;
+      newReturnState[action.step.id] = action.step;
       return newReturnState;
      case REMOVE_STEP:
-      const newState = Object.assign({}, state);
       delete newState[action.step.id];
       return newState;
     default:
       return state; 
   }
 }
+
 export default stepsReducer;
